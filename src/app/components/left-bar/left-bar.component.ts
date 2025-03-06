142dee59daa824fe4d60e6ae2b3609369c32c1dd
lucasb <lkab05@hotmail.com>
2025-03-05 22:10:01 -0300

    feat: added left bar layer

---
+++ src/app/components/left-bar/left-bar.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+import { MenuButtonComponent } from '../menu-button/menu-button.component';
+import {
+  faGuitar,
+  faHome,
+  faMusic,
+  faSearch,
+} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { IPlaylist } from '../../interfaces/IPlaylist';
+import { SpotifyService } from '../../services/spotify.service';
+import { CommonModule } from '@angular/common';
+import { FooterComponent } from '../footer/footer.component';
+
+@Component({
+  selector: 'app-left-bar',
+  imports: [
+    MenuButtonComponent,
+    FontAwesomeModule,
+    CommonModule,
+    FooterComponent,
+  ],
+  templateUrl: './left-bar.component.html',
+  styleUrl: './left-bar.component.scss',
+})
+export class LeftBarComponent implements OnInit {
+  homeIcon = faHome;
+  searchIcon = faSearch;
+  artistIcon = faGuitar;
+  playlistIcon = faMusic;
+
+  menuSelected = 'Home';
+
+  playlists: IPlaylist[] = [];
+
+  constructor(private spotifyService: SpotifyService) {}
+
+  ngOnInit(): void {
+    this.searchPlaylists();
+  }
+
+  buttonClick(button: string) {
+    this.menuSelected = button;
+  }
+
+  async searchPlaylists() {
+    this.playlists = await this.spotifyService.getPlaylistsByUser();
+  }
+}
