5b820efb10398c961a80893c2391bb4341d1e86f
lucasb <lkab05@hotmail.com>
2025-03-09 22:14:49 -0300

    feat: add methods for retrieving top artists and saved tracks in SpotifyService

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -1,10 +1,17 @@
 import { Injectable } from '@angular/core';
-import { SpotifyConfig } from '../../environments/environment';
 import Spotify from 'spotify-web-api-js';
 import { IUser } from '../interfaces/IUser';
-import { PlaylistSpotifyDTO, UserSpotifyDTO } from '../commoms/helper';
+import {
+  PlaylistSpotifyDTO,
+  SpotifyArtistDTO,
+  SpotifyTrackDTO,
+  UserSpotifyDTO,
+} from '../commoms/helper';
 import { IPlaylist } from '../interfaces/IPlaylist';
 import { Router } from '@angular/router';
+import { IArtist } from '../interfaces/IArtist';
+import { SpotifyConfig } from '../../environments/environment';
+import { IMusic } from '../interfaces/IMusic';
 
 @Injectable({
   providedIn: 'root',
@@ -75,6 +82,31 @@ export class SpotifyService {
     console.log(playlists);
 
     return playlists.items.map((item) => PlaylistSpotifyDTO(item));
+  }
+
+  async getTopArtists(limit = 10, time_range: 'long_term'): Promise<IArtist[]> {
+    const artists = await this.spotifyApi.getMyTopArtists({
+      limit,
+      time_range,
+    });
+    console.log(artists);
+
+    return artists.items.map((x) => SpotifyArtistDTO(x));
+  }
+
+  async getMusics(offset = 0, limit = 50): Promise<IMusic[]> {
+    const musics = await this.spotifyApi.getMySavedTracks({ offset, limit });
+
+    return musics.items.map((music) => SpotifyTrackDTO(music.track));
+  }
+
+  async playMusic(musicId: string) {
+    await this.spotifyApi.queue(musicId);
+    await this.spotifyApi.skipToNext();
+  }
+  async getActualMusic(): Promise<IMusic> {
+    const music = await this.spotifyApi.getMyCurrentPlayingTrack();
+    return SpotifyTrackDTO(music.item);
   }
 
   logout() {
